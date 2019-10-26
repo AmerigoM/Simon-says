010a1d4eb6a4aae89583cdc416db501b761905fc
rexam14 <amerigo.mancino@gmail.com>
2019-10-26 17:31:14 +0200

    Button highlight animation created.

--- Simon says/CircularButton.swift
+++ Simon says/CircularButton.swift
@@ -14,9 +14,25 @@ class CircularButton: UIButton {
     // since we don't have access to the viewDidLoad method, we
     // have to use the corresponding function for UI elements
     override func awakeFromNib() {
+         super.awakeFromNib()
+        
         // make sure the button is circular
         layer.cornerRadius = frame.size.width/2
         layer.masksToBounds = true
+    }
+    
+    // computed property that modify the alpha value
+    // and gives the buttons an animation when thy are touched
+    override var isHighlighted: Bool {
+        // every time this property is set (which happens
+        // when the user taps on a button)...
+        didSet {
+            if isHighlighted {
+                alpha = 1.0
+            } else {
+                alpha = 0.5
+            }
+        }
     }
 
 }
--- Simon says/ViewController.swift
+++ Simon says/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorButtons: [CircularButton]!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var playerLabels: [UILabel]!
+    @IBOutlet var scoreLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // order the colorButtons array by tag
+        colorButtons = colorButtons.sorted(by: { (btn1, btn2) -> Bool in
+            btn1.tag < btn2.tag
+        })
+        
+        // order the player label array by tag
+        playerLabels = playerLabels.sorted(by: { (lab1, lab2) -> Bool in
+            lab1.tag < lab2.tag
+        })
+        
+        // order the scoreLabel array by tag
+        scoreLabels = scoreLabels.sorted(by: { (lab1, lab2) -> Bool in
+            lab1.tag < lab2.tag
+        })
+        
+        // create a new game
+        createNewGame()
     }
 
-
+    @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        print("Button \(sender.tag) tapped!")
+    }
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+        print("Action Button")
+    }
+    
+    func createNewGame() {
+        // change the action button text
+        actionButton.setTitle("Start Game", for: .normal)
+        
+        // reduce the opacity of all the color buttons
+        for button in colorButtons {
+            button.alpha = 0.5
+        }
+    }
 }
 
